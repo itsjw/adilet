d2cb490e87a40a0a80f76b50a9ad8604496175b1
Zoxon470 <zoxon470@gmail.com>
2017-10-21 23:30:53 +0600

    Changed Models

--- jardam/help/models.py
+++ jardam/help/models.py
@@ -5,6 +5,10 @@ class HelpCategory(models.Model):
     category_name = models.CharField(max_length=255, verbose_name='Название категории')
     article = models.ForeignKey('Article', related_name='article', null=True, verbose_name='Статья')
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
     def __str__(self):
         return self.category_name
 
@@ -13,6 +17,10 @@ class Article(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
     description = models.CharField(max_length=255, verbose_name='Описание')
     tag = models.CharField(max_length=255, verbose_name='Тэг')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
 
     def __str__(self):
         return self.title
