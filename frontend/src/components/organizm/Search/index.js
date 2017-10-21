5af3a54bedb852293732484dc56806f4d622f1dd
ketrin <ketrin01110101@gmail.com>
2017-10-21 10:17:45 +0300

    modified search section

--- frontend/src/components/organizm/Search/index.js
+++ frontend/src/components/organizm/Search/index.js
@@ -2,13 +2,36 @@ import React from 'react'
 import injectGlobal from 'styled-components'
 
 injectGlobal`
-
+.search{
+  margin: 0;
+}
+.search-header{
+  margin:0 auto;
+}
+.search-input{
+  margin: 0 auto;
+}
+.search-input input{
+  width: 550px;
+  height: 40px;
+}
+.btn{
+  height: 40px;
+  padding: 0 5px;
+  width: 70px;
+}
 `
 
 const Search = () => {
   return (
-    <div>
-      Search
+    <div className="search">
+      <div className="search-header">
+        <h1>search-header search-header search-header</h1>
+      </div>
+      <div className="search-input">
+        <input type="text" placeholder="searching area">
+        <button className="btn">Search</button>
+      </div>
     </div>
   )
 }
