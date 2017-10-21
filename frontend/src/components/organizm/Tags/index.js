b970450e4a8c936006e345884cb577f284a80ccb
ketrin <ketrin01110101@gmail.com>
2017-10-21 10:46:39 +0300

    modified tags section

--- frontend/src/components/organizm/Tags/index.js
+++ frontend/src/components/organizm/Tags/index.js
@@ -2,13 +2,57 @@ import React from 'react'
 import injectGlobal from 'styled-components'
 
 injectGlobal`
-
+.tags{
+  margin:0;
+  height: auto;
+}
+.tags-item{
+  margin-bottom: 30px;
+  max-width: 200px;
+  height: 40px;
+  border-radius: 30px;
+}
 `
 
 const Tags = () => {
   return (
-    <div>
-      Tags
+    <div className="tags">
+      <div className="tags-items">
+      <div className="container">
+
+        <div className="tags-item col-lg-2">
+          <p>keker</p>
+        <div>
+        <div className="tags-item col-lg-2">
+          <p>lolol</p>
+        <div>
+        <div className="tags-item col-lg-3">
+          <p>govno s percom</p>
+        <div>
+        <div className="tags-item col-lg-2">
+          <p>furrrva</p>
+        <div>
+        <div className="tags-item col-lg-2">
+          <p>piezdola</p>
+        <div>
+        <div className="tags-item col-lg-2">
+          <p>keker suka</p>
+        <div>
+        <div className="tags-item col-lg-3">
+          <p>keker bliat</p>
+        <div>
+        <div className="tags-item col-lg-2">
+          <p>superman</p>
+        <div>
+        <div className="tags-item col-lg-2">
+          <p>kal el</p>
+        <div>
+        <div className="tags-item col-lg-2">
+          <p>keker mmmm</p>
+        <div>
+
+      </div>
+      </div>
     </div>
   )
 }
