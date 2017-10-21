316e81700db6facf480a7d38b296b41f7aad7c07
kkamagwi <smolyaevaekaterina@gmail.com>
2017-10-21 19:42:10 +0300

    more charecters

--- jardam/education/models.py
+++ jardam/education/models.py
@@ -11,7 +11,7 @@ class EducationCategory(models.Model):
 
 class Education(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название')
-    content = models.CharField(max_length=255, verbose_name='Контент')
+    content = models.CharField(max_length=2000, verbose_name='Контент')
     tag = models.CharField(max_length=255, verbose_name='Тэг')
 
     def __str__(self):
