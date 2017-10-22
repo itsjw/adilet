31cc7203f45b58310756d1c8b4acbdd6b4d08dc0
Zoxon470 <zoxon470@gmail.com>
2017-10-22 16:36:40 +0600

    Changed Search

--- jardam/main/views.py
+++ jardam/main/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from help.models import Article
 
 def search(request, name_article):
-    search_list = Article.objects.all().filter(title=name_article)
+    search_list = Article.objects.all().filter(title__startswith=name_article)
     print(search_list)
     return render(request, 'main/result.html', {'search_list': search_list})
