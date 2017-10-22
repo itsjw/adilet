0618bc80364ba55572e9041a5f4c75e1a3ed26d1
Zoxon470 <zoxon470@gmail.com>
2017-10-22 16:49:15 +0600

    Added startswith to search

--- jardam/main/views.py
+++ jardam/main/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.db.models import Q
 from help.models import Article
 
+
+
 def search(request, name_article):
-    search_list = Article.objects.all().filter(title__startswith=name_article)
+    search_list = Article.objects.all().filter(
+        Q(title=name_article) and
+        Q(title__startswith=name_article)
+        )
     print(search_list)
     return render(request, 'main/result.html', {'search_list': search_list})
+
+
